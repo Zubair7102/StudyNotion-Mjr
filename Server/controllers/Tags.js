5e86dac56f7047ec353f939e7b629cef7eaf9acc
Mohd Zubair Ahmed <jubaira646@gmail.com>
2024-09-04 00:48:22 +0530

    createTag function

--- Server/controllers/Tags.js
+++ Server/controllers/Tags.js
@@ -1,1 +1,38 @@
 const Tag = require("../models/Tag");
+
+// create Tag handler function
+
+exports.createTag = async (req, res) =>{
+    try{
+        // fetching data from the req body
+        const {name, description} = req.body;
+        // validate
+        if(!name || !description)
+        {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required",
+            })
+        }
+
+        // create entry in db
+        const tagDetails = await Tag.create({
+            name: name,
+            description: description,
+        })
+        console.log(tagDetails);
+        res.status(201).json({
+            success: true,
+            message: "Tag created successfully",
+            Tag: tagDetails,
+        })
+    }
+    catch(error)
+    {
+        console.log("Error creating tag: ", error.message);
+        res.status(500).json({
+            success: false,
+            message: "Error creating tag",
+        })
+    }
+}
