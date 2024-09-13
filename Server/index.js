9f0a32e62a98366839db08a8e7286e7ad5966dc1
Mohd Zubair Ahmed <jubaira646@gmail.com>
2024-09-14 01:03:02 +0530

    added routes middleware and required functions

--- Server/index.js
+++ Server/index.js
@@ -1,18 +1,57 @@
 const express = require("express");
 const app = express();
-
 require("dotenv").config();
+
+const userRoutes = require("./routes/User");
+const profileRoutes = require("./routes/Profile");
+const paymentRoutes = require("./routes/Payments");
+const courseRoutes = require("./routes/Course");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+// connect with cloud
+const cloudinary = require("./config/cloudinary");
+cloudinary.cloudinaryConnect();
+const fileupload = require("express-fileupload");
+require("./config/database").dbConnect();
 
 const PORT = process.env.PORT || 4000;
 
-app.use(express.json());
-require("./config/database").dbConnect();
 
-// here route is yet to be defined 
-const route = require("./routes/");
-app.use('/api/v1/route', route);
+// add middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+        // this function is used to respond to all the request recieved from frontend
+    })
+)
+
+// cloudinary ***
+app.use(fileupload(
+    {
+        useTempFiles : true,
+        tempFileDir : '/tmp/'
+    }
+));
+
+
+// routes
+app.use('/api/v1/auth', userRoutes);
+app.use('/api/v1/profile', profileRoutes);
+app.use('/api/v1/course', courseRoutes);
+app.use("/api/v1/payment", paymentRoutes);
+
+// default route 
+app.get("/", (req, res)=>{
+    return res.json({
+    success: true,
+    message: 'Your Server is Up and running.....'
+})
+}) 
 
 // activate the server
 app.listen(PORT, ()=>{
-    console.log(`APP is listening at the port ${PORT}`)
+    console.log(`App is listening at the port ${PORT}...`)
 })
